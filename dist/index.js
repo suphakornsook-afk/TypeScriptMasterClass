956120196353eb394e00680c3055de6daa3c582d
Suphakorn <suphakorn.sook@gmail.com>
2026-08-17 10:41:26 +0700

    try Type Aliases

--- dist/index.js
+++ dist/index.js
@@ -36,4 +36,22 @@ function createPost(post) {
     console.log(`Created post ${post.title} by ${post.author.name}`);
 }
 createPost(newPost);
+let posts = [];
+posts.push(newPost);
+console.log(`${posts.length}`);
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
+const userOne = { name: 'mario', score: Math.floor(Math.random() * 100) };
+function formatUser(user) {
+    console.log(`${user.name} has a ${user.score} score`);
+}
+formatUser(userOne);
+formatUser({ name: 'Warrio', score: 45 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -64,3 +64,38 @@ function createPost(post:Post): void{
 }
 
 createPost(newPost);
+
+let posts: Post[] = []
+posts.push(newPost)
+console.log(`${posts.length}`)
+
+
+
+type Rgb = [number,number,number]
+
+function getRandomColor(): Rgb {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+
+    return [r,g,b]
+}
+
+const colorOne = getRandomColor()
+const colorTwo = getRandomColor()
+console.log(colorOne,colorTwo)
+
+
+type User = {
+    name: string,
+    score: number,
+}
+
+const userOne: User = {name: 'mario', score: Math.floor(Math.random() * 100)}
+
+function formatUser(user:User): void{
+    console.log(`${user.name} has a ${user.score} score`)
+}
+
+formatUser(userOne)
+formatUser({name: 'Warrio',score: 45})
